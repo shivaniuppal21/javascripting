1140501f29fab948a1d64035430f73f81a5f597d
shivaniuppal21 <shivaniuppal21@gmail.com>
2020-11-10 15:00:26 -0500

    Initial commit with all my work so far

---
+++ for-loop.js
@@ -0,0 +1,6 @@
+let variable = 0
+let limit = 10
+for(let i=0; i<10;i++)
+{
+    console.log(variable += i)
+}
---
+++ string-length.js
@@ -0,0 +1,3 @@
+const example = 'example string' 
+console.log(example.length)
+javascripting verify string-length.js 
